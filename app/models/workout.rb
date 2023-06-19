5662e3e4079bb787d15006915b9be87c0f0a7ae8
Jethro_Block <jethroblock22@gmail.COM>
2023-06-19 16:11:36 +0100

    Add Associations to workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,7 @@
 class Workout < ApplicationRecord
   belongs_to :user
+  has_many :routine_workouts
+  has_many :routines, through: :routine_workouts
+  has_many :workout_exercises
+  has_many :exercises, through: :workout_exercises
 end
