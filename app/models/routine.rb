fbbc603c53178636212c5e97f9395a15e506f8b7
Jethro_Block <jethroblock22@gmail.COM>
2023-06-23 15:12:17 +0100

    Add remove workout schedule, dependent destroy to Routine model

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,5 +1,15 @@
 class Routine < ApplicationRecord
   belongs_to :user
-  has_many :routine_workouts
+  has_many :routine_workouts, dependent: :destroy
   has_many :workouts, through: :routine_workouts
+
+after_destroy :remove_future_workout_schedules
+
+def remove_future_workout_schedules
+  self.routine_workouts.each do |routine_workout|
+    WorkoutSchedule.where("date > ? AND routine_workout_id = ?", Date.today, routine_workout.id).destroy_all
+end
+end 
+end
+
 end
