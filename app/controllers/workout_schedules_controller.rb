57a956f1703566c2b161f4c1672af55e4bf12218
Jethro_Block <jethroblock22@gmail.COM>
2023-06-23 12:39:26 +0100

    Add methods for workout_schedules

--- app/controllers/workout_schedules_controller.rb
+++ app/controllers/workout_schedules_controller.rb
@@ -1,2 +1,53 @@
 class WorkoutSchedulesController < ApplicationController
+
+    before_action :set_workout_schedule, only: [:show, :update, :destroy]
+
+    # GET /workout_schedules
+    def index
+      @workout_schedules = WorkoutSchedule.all
+  
+      render json: @workout_schedules
+    end
+  
+    # GET /workout_schedules/1
+    def show
+      render json: @workout_schedule
+    end
+  
+    # POST /workout_schedules
+    def create
+      @workout_schedule = WorkoutSchedule.new(workout_schedule_params)
+  
+      if @workout_schedule.save
+        render json: @workout_schedule, status: :created
+      else
+        render json: @workout_schedule.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /workout_schedules/1
+    def update
+      if @workout_schedule.update(workout_schedule_params)
+        render json: @workout_schedule
+      else
+        render json: @workout_schedule.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /workout_schedules/1
+    def destroy
+      @workout_schedule.destroy
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_workout_schedule
+        @workout_schedule = WorkoutSchedule.find(params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def workout_schedule_params
+        params.require(:workout_schedule).permit(:user_id, :routine_workout_id, :date)
+      end
+
 end
