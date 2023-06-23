96e71f8867d8372b0fc78ae4345435da8d2e6f71
Jethro_Block <jethroblock22@gmail.COM>
2023-06-23 15:58:40 +0100

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_105852) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_145746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,6 +99,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_105852) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
     t.index ["routine_workout_id"], name: "index_workout_schedules_on_routine_workout_id"
     t.index ["user_id"], name: "index_workout_schedules_on_user_id"
   end
