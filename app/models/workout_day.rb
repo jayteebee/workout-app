fb278a67913421f392e9cfa0639c956d2c40dada
Jethro_Block <jethroblock22@gmail.COM>
2023-06-23 15:14:08 +0100

    Add remove workout schedules to workout day model

--- app/models/workout_day.rb
+++ app/models/workout_day.rb
@@ -1,3 +1,10 @@
 class WorkoutDay < ApplicationRecord
   belongs_to :user
+
+after_destroy :remove_future_workout_schedules
+
+def remove_future_workout_schedules
+  self.user.workout_schedules.where("date > ?", Date.today).destroy_all
+end
+
 end
