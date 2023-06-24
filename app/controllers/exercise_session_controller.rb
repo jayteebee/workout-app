2fb68ac1aa40afc59e52ad47d4282fac3b5efba3
Jethro_Block <jethroblock22@gmail.COM>
2023-06-24 17:19:04 +0100

    Populate basic routes from workout_session to exercise_session Modify index

--- app/controllers/exercise_session_controller.rb
+++ app/controllers/exercise_session_controller.rb
@@ -1,2 +1,57 @@
 class ExerciseSessionController < ApplicationController
+
+    before_action :set_exercise_session, only: [:show, :update, :destroy ]
+
+    # GET REQUESTS
+    # /exercise_session
+    def index
+        @exercise_sessions = ExerciseSession.all
+        render json: @exercise_sessions
+    end
+
+    # /workout_session/1
+    def show
+        render json: @workout_session
+    end
+
+    # POST REQUESTS
+    # /workout_session
+    def create
+        @workout_session = WorkoutSession.new(workout_session_params)
+
+        if @workout_session.save
+            render json: @workout_session, status: :created
+          else
+            render json: @workout_session.errors, status: :unprocessable_entity
+          end
+    end
+
+
+    # PATCH/PUT REQUESTS
+    # /workout_session/1
+    def update
+        if @workout_session.update(workout_session_params)
+            render json: @workout_session, status: :ok
+          else
+            render json: @workout_session.errors, status: :unprocessable_entity
+          end
+    end
+
+# DELETE REQUESTS   
+# /workout_session/1
+    def destroy
+        @workout_session.destroy
+    end
+
+    
+    private
+
+    def set_workout_session
+        @workout_session = WorkoutSession.find(params[:id])
+    end
+
+    def workout_session_params
+        params.require(:workout_session).permit(:user_id, :workout_id, :date, :total_duration)
+      end
+
 end
