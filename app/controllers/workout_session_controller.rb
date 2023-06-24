005f8ef16f923ad0a3930f6583c26f718b6ff25c
Jethro_Block <jethroblock22@gmail.COM>
2023-06-24 16:38:58 +0100

    Add method shells

--- app/controllers/workout_session_controller.rb
+++ app/controllers/workout_session_controller.rb
@@ -1,2 +1,30 @@
 class WorkoutSessionController < ApplicationController
+
+    def index
+
+    end
+
+
+    def show
+
+    end
+
+
+    def create
+
+    end
+
+
+    def update
+
+    end
+
+
+    def destroy
+
+    end
+
+    
+    
+
 end
