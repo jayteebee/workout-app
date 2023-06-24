b7e7f7e2d0713ba9edc3aff7936a654e9b75e6e4
Jethro_Block <jethroblock22@gmail.COM>
2023-06-24 14:19:55 +0100

    Add dependent::destroy on workout model / workout_exercises

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,6 +2,6 @@ class Workout < ApplicationRecord
   belongs_to :user
   has_many :routine_workouts
   has_many :routines, through: :routine_workouts
-  has_many :workout_exercises
+  has_many :workout_exercises, dependent: :destroy
   has_many :exercises, through: :workout_exercises
 end
