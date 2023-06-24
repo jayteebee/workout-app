e3f971bb925b5bfe1137a5fbf450ba76b46de10e
Jethro_Block <jethroblock22@gmail.COM>
2023-06-24 17:03:25 +0100

    Add workout_session_params private method

--- app/controllers/workout_session_controller.rb
+++ app/controllers/workout_session_controller.rb
@@ -1,18 +1,21 @@
 class WorkoutSessionController < ApplicationController
 
+    before_action :set_workout_session, only: [:show ]
+
     # GET REQUESTS
-    # /workout_sessions
+    # /workout_session
     def index
         @workout_sessions = WorkoutSession.all
         render json: @workout_sessions
     end
 
-
+    # /workout_session/1
     def show
-
+        render json: @workout_session
     end
 
-
+    # POST REQUESTS
+    # /workout_session
     def create
 
     end
@@ -34,5 +37,7 @@ class WorkoutSessionController < ApplicationController
         @workout_session = WorkoutSession.find(params[:id])
     end
 
-
+    def workout_session_params
+        params.require(:workout_session).permit(:user_id, :workout_id, :date, :total_duration)
+      end
 end
