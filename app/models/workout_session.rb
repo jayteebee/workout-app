383bdea678502190223da60c997d1adcceb06a9b
Jethro_Block <jethroblock22@gmail.COM>
2023-06-24 17:44:58 +0100

    Add json return option to display exercise_session

--- app/models/workout_session.rb
+++ app/models/workout_session.rb
@@ -2,4 +2,9 @@ class WorkoutSession < ApplicationRecord
   belongs_to :workout
   belongs_to :user
   has_many :exercise_sessions, dependent: :destroy
+
+
+  def as_json(options={})
+    super(options.merge(include: :exercise_sessions))
+  end
 end
