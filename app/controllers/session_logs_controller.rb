6485a5b779deec3711c7633c64c2a41d1405b6d6
Jethro_Block <jethroblock22@gmail.COM>
2023-07-02 15:53:58 +0100

    Update session logs controller

--- app/controllers/session_logs_controller.rb
+++ app/controllers/session_logs_controller.rb
@@ -1,29 +1,44 @@
 class SessionLogsController < ApplicationController
-before_action :set_session_log, only: [:show, :update, :destroy ]
+  before_action :set_session_log, only: [:show, :update, :destroy]
 
-# GET REQUEST
-# /session_logs
   def index
-    @session_log = SessionLog.all
-    
-    render json: @session_log
+    @session_logs = SessionLog.all
+    render json: @session_logs
   end
 
   def show
+    render json: @session_log
   end
 
   def create
+    @session_log = SessionLog.new(session_log_params)
+    if @session_log.save
+      render json: @session_log, status: :created
+    else
+      render json: @session_log.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @session_log.update(session_log_params)
+      render json: @session_log
+    else
+      render json: @session_log.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @session_log.destroy
+    render json: {message: 'Session log successfully deleted'}, status: :ok
   end
 
-  private 
+  private
 
   def set_session_log
     @session_log = SessionLog.find(params[:id])
+  end
+
+  def session_log_params
+    params.require(:session_log).permit(:user_id, :details)
   end
 end
