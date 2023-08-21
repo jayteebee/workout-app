17af19337ca31323a37d4efa1ff02a241e8dcb1a
Jethro_Block <jethroblock22@gmail.COM>
2023-08-21 18:06:44 +0100

    Pass params to WSRJob

--- app/services/workout_schedule_regeneration_job.rb
+++ app/services/workout_schedule_regeneration_job.rb
@@ -1,9 +1,10 @@
 class WorkoutScheduleRegenerationJob
 include Sidekiq::Worker
 
-def perform
-    User.find_each do |user|
-        WorkoutScheduleGenerator.new(user).generate
+def perform(user_id, routine_id)
+    user = User.find_by(id: user_id)
+    return unless user
+        WorkoutScheduleGenerator.new(user).generate_schedules(routine_id)
     end
 end
 end
