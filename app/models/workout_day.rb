ce7e1aa6201655b3956341ae47fc01d252395945
Jethro_Block <jethroblock22@gmail.COM>
2023-08-30 16:54:32 +0100

    delete routine via routine not user

--- app/models/workout_day.rb
+++ app/models/workout_day.rb
@@ -5,7 +5,9 @@ class WorkoutDay < ApplicationRecord
 after_destroy :remove_future_workout_schedules
 
 def remove_future_workout_schedules
-  self.user.workout_schedules.where("date > ?", Date.today).destroy_all
+  self.routine.workout_schedules.where("date > ?", Date.today).destroy_all
 end
 
 end
+
+# self.user.workout_schedules.where("date > ?", Date.today).destroy_all
