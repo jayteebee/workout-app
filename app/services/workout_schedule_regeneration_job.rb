28ac4f19e9ea79bb50cc007f9c869e8d38959acf
Jethro_Block <jethroblock22@gmail.COM>
2023-08-30 16:53:58 +0100

    remove comment

--- app/services/workout_schedule_regeneration_job.rb
+++ app/services/workout_schedule_regeneration_job.rb
@@ -6,8 +6,6 @@ sidekiq_options queue: 'default', retry: true
 def perform(user_id, routine_id)
     user = User.find_by(id: user_id)
     return unless user
-    puts "***** HERE 2 HERE 2 HERE ***** (#{user_id}, #{routine_id}) "
-    
         WorkoutScheduleGenerator.new(user).generate_schedules(routine_id)
     end
 end
