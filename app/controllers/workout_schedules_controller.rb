31ebd79f9e8f6ec5ba81dd3a66d6f105ccd93173
Jethro_Block <jethroblock22@gmail.COM>
2023-09-28 15:13:47 +0100

    add query scoping for current user

--- app/controllers/workout_schedules_controller.rb
+++ app/controllers/workout_schedules_controller.rb
@@ -1,11 +1,12 @@
 class WorkoutSchedulesController < ApplicationController
 
     before_action :set_workout_schedule, only: [:show, :update, :destroy]
+    before_action :authenticate_user!
 
     # GET REQUESTS
     #  /workout_schedules
     def index
-      @workout_schedules = WorkoutSchedule.includes(routine_workout: :workout).all
+      @workout_schedules = current_user.workout_schedules.includes(routine_workout: :workout).all
   
       response_data = @workout_schedules.map do |schedule|
         {
@@ -25,24 +26,32 @@ class WorkoutSchedulesController < ApplicationController
   
     # /workout_schedules/1
     def show
+      if @workout_schedule.user_id == current_user.id
       render json: @workout_schedule
+      else
+        render json: {error: "Unauthorized: User ID does not match current user ID"}, status: :Unauthorized
+      end
     end
   
     # fetch all workoutschedule entries for a user within the date range
 def user_schedules
   start_date = params[:start_date].to_date
-end_date = params[:end_date].to_date
-user = User.find(params[:id])
-@workout_schedules = user.workout_schedules.where(date: start_date..end_date)
+  end_date = params[:end_date].to_date
+  user = User.find(params[:id])
 
-render json: @workout_schedules, status: :ok
+  if user == current_user
+    @workout_schedules = user.workout_schedules.where(date: start_date..end_date)
+      render json: @workout_schedules, status: :ok
+  else
+    render json: {error: "Unauthorized: User does not match current user"}, status: :Unauthorized
+  end
 end
 
 
     # POST REQUESTS
     #  /workout_schedules
     def create
-      @workout_schedule = WorkoutSchedule.new(workout_schedule_params)
+      @workout_schedule = current_user.workout_schedules.new(workout_schedule_params)
   
       if @workout_schedule.save
         render json: @workout_schedule, status: :created
@@ -51,23 +60,29 @@ end
       end
     end
 
-
-
     # PATCH/PUT REQUESTS
     #  /workout_schedules/1
     def update
-      if @workout_schedule.update(workout_schedule_params)
-        render json: @workout_schedule, status: :ok
+      if @workout_schedule.user_id == current_user.id
+        if @workout_schedule.update(workout_schedule_params)
+          render json: @workout_schedule, status: :ok
+        else
+          render json: @workout_schedule.errors, status: :unprocessable_entity
+       end
       else
-        render json: @workout_schedule.errors, status: :unprocessable_entity
-      end
+        render json: {error: "Unauthorized: User ID does not match current user ID"}, status: :Unauthorized
+     end
     end
   
     # DELETE REQUESTS
     #  /workout_schedules/1
     def destroy
+      if @workout_schedule.user_id == current_user.id
       @workout_schedule.destroy
+      else
+        render json: {error: "Unauthorized: User ID does not match current user ID"}, status: :Unauthorized
     end
+  end
   
     private
 
