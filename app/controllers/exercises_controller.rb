5927ad7303ad34f2b21cd9fed87f221df6aec8b9
Jethro_Block <jethroblock22@gmail.COM>
2023-09-28 16:04:29 +0100

    add user query scoping

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,21 +1,25 @@
 class ExercisesController < ApplicationController
-  before_action :set_exercise, only: %i[ show update destroy ]
+  before_action :set_exercise, only: %i[show update destroy]
+  before_action :authenticate_user!
 
   # GET /exercises
   def index
-    @exercises = Exercise.all
-
+    @exercises = current_user.exercises
     render json: @exercises
   end
 
   # GET /exercises/1
   def show
-    render json: @exercise
+    if @exercise.user_id == current_user.id
+      render json: @exercise
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
   end
 
   # POST /exercises
   def create
-    @exercise = Exercise.new(exercise_params)
+    @exercise = current_user.exercises.build(exercise_params)
 
     if @exercise.save
       render json: @exercise, status: :created, location: @exercise
@@ -26,26 +30,34 @@ class ExercisesController < ApplicationController
 
   # PATCH/PUT /exercises/1
   def update
-    if @exercise.update(exercise_params)
-      render json: @exercise
+    if @exercise.user_id == current_user.id
+      if @exercise.update(exercise_params)
+        render json: @exercise
+      else
+        render json: @exercise.errors, status: :unprocessable_entity
+      end
     else
-      render json: @exercise.errors, status: :unprocessable_entity
+      render json: { error: 'Unauthorized' }, status: :unauthorized
     end
   end
 
   # DELETE /exercises/1
   def destroy
-    @exercise.destroy
+
+    if @exercise.user_id == current_user.id
+      @exercise.destroy
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_exercise
-      @exercise = Exercise.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def exercise_params
-      params.require(:exercise).permit(:name, :sets, :reps, :weight, :duration, :personal_best)
-    end
+  def set_exercise
+    @exercise = Exercise.find(params[:id])
+  end
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :sets, :reps, :weight, :duration, :personal_best)
+  end
 end
