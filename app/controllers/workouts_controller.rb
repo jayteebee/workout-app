dfb072f62d7560dab0fb8154992ddae25587d098
Jethro_Block <jethroblock22@gmail.COM>
2023-09-28 15:55:26 +0100

    add user query scoping

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,36 +1,43 @@
 class WorkoutsController < ApplicationController
   before_action :set_workout_for_show_update_destroy, only: %i[ show update destroy ]
   before_action :set_workout_for_nested_resources, only: %i[ exercises add_exercise delete_exercise update_exercise ]
+  before_action :authenticate_user!
 
   # GET Requests
   # /workouts
   def index
-    @workouts = Workout.all
-
+    @workouts = current_user.workouts
     render json: @workouts
   end
 
   # /workouts/1
   def show
-    render json: @workout
+    if @workout.user_id == current_user.id
+      render json: @workout
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
   end
 
   # /workouts/1/exercises
 def exercises
+  if @workout.user_id == current_user.id
   workout_exercises = @workout.workout_exercises.includes(:exercise)
   workout_exercises_as_json = workout_exercises.as_json(include: :exercise)
 
   workout_exercises_as_json.each do |workout_exercise|
     workout_exercise["workout_name"] = @workout.name
   end
-
   render json: workout_exercises_as_json
+else
+  render json: { error: 'Unauthorized' }, status: :unauthorized
+end
 end
 
   # POST REQUESTS
   # /workouts
   def create
-    @workout = Workout.new(workout_params)
+    @workout = current_user.workouts.new(workout_params)
 
     if @workout.save
       render json: @workout, status: :created, location: @workout
@@ -42,15 +49,21 @@ end
   # PATCH/PUT REQUESTS
   # /workouts/1
   def update
+    if @workout.user_id == current_user.id
     if @workout.update(workout_params)
       render json: @workout
     else
       render json: @workout.errors, status: :unprocessable_entity
     end
+  else
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  end
   end
 
   # /workouts/1/exercises/1
   def update_exercise
+    if @workout.user_id == current_user.id
+
     @exercise = Exercise.find(params[:exercise_id])
     workout_exercise = @workout.workout_exercises.find_by(exercise: @exercise)
     if workout_exercise.nil?
@@ -66,10 +79,15 @@ end
         render json: workout_exercise.errors, status: :unprocessable_entity
       end
     end
+  else
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  end
   end
 
   # /workouts/1/exercises
 def add_exercise
+  if @workout.user_id == current_user.id
+
   @exercise = Exercise.find(params[:exercise_id])
   workout_exercise = WorkoutExercise.new(workout: @workout, exercise: @exercise)
   workout_exercise.sets = params[:sets]
@@ -83,16 +101,24 @@ def add_exercise
   else
     render json: workout_exercise.errors, status: :unprocessable_entity
 end
+else
+  render json: { error: 'Unauthorized' }, status: :unauthorized
+end
 end
 
   # DELETE REQUESTS
   # /workouts/1
   def destroy
-    @workout.destroy
+    if @workout.user_id == current_user.id
+      @workout.destroy
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
   end
 
   # /workouts/1/exercises/1
   def delete_exercise
+    if @workout.user_id == current_user.id
     workout_exercise = WorkoutExercise.find_by(workout: @workout, exercise: params[:exercise_id])
     if workout_exercise
     workout_exercise.destroy
@@ -100,6 +126,9 @@ end
     else
       render json: {error: "Exercise not found in this workout"}, status: :not_found
   end
+else
+  render json: { error: 'Unauthorized' }, status: :unauthorized
+end
 end
  
   # def delete_exercise
