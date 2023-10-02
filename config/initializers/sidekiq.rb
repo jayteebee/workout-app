ba394fb599855837c80a631be259450bf1d5c5e6
Jethro_Block <jethroblock22@gmail.COM>
2023-10-02 11:58:11 +0100

    change cron job execution time

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,12 +3,12 @@ require "sidekiq-cron"
 
 Sidekiq::Cron::Job.create(
     name: "WorkoutSchedule regeneration - every day",
-    cron: "*/5 * * * *",
+    cron: "0 0 * * *",
     class: "WorkoutScheduleRegenerationJob"
 )
 
 Sidekiq::Cron::Job.create(
     name: "WorkoutSchedule Frequency regeneration - every day",
-    cron: "*/5 * * * *", 
+    cron: "0 0 * * *",
     class: "WorkoutScheduleFrequencyRegenerationJob" 
 )
