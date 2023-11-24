306be2c453376998a7ac9cfa50986559c44c8251
Jethro_Block <jethroblock22@gmail.COM>
2023-11-24 14:43:05 +0000

    syntax

--- app/controllers/session_logs_controller.rb
+++ app/controllers/session_logs_controller.rb
@@ -104,6 +104,6 @@ class SessionLogsController < ApplicationController
   end
 
   def session_log_params
-    params.require(:session_log).permit(:user_id, details: {}, :workout_name)
+    params.require(:session_log).permit(:user_id, :workout_name, details: {}, )
   end
 end
