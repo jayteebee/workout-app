27dffb7b8228e46317adc08fb227d0499d4a7657
Jethro_Block <jethroblock22@gmail.COM>
2023-12-28 12:57:19 +0000

    add sendgrid and email confirmation #1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
     include Devise::JWT::RevocationStrategies::JTIMatcher
 
     devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable,
+    :recoverable, :rememberable, :validatable, :confirmable,
     :jwt_authenticatable, jwt_revocation_strategy: self
 
     validates_format_of :email, with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"
     validates :email, uniqueness: true
-    
+
     has_many :routines
     has_many :workouts
     has_many :workout_days
