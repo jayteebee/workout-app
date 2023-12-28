f90f37d2ef399d2cbfead1a10e86382cbdac5250
Jethro_Block <jethroblock22@gmail.COM>
2023-12-28 18:17:09 +0000

    Refactor confirm_account method to return JSON response

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -5,16 +5,16 @@ class ConfirmationsController < Devise::ConfirmationsController
 #   def show
 def confirm_account
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-
+  
     if resource.errors.empty?
-      set_flash_message(:notice, :confirmed) if is_flashing_format?
-      # Redirect to the front end confirmation success page
-      redirect_to "https://workout-app-fe-five.vercel.app/GettingStarted"
+      # Return a JSON response instead of redirecting
+      render json: { status: 'success', message: 'Account confirmed successfully' }, status: :ok
     else
-      # Redirect to the front end error page or pass the error messages
-      redirect_to "https://workout-app-fe-five.vercel.app/confirmation_error?errors=#{resource.errors.full_messages.join(',')}"
+      # Return error details in the JSON response
+      render json: { status: 'error', errors: resource.errors.full_messages }, status: :unprocessable_entity
     end
   end
+  
 
     # def after_confirmation_path_for(resource_name, resource)
     #     'localhost:3000/'
