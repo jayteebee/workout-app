1bec853ad9436a449b60c8a75b88c11758e31ffb
Jethro_Block <jethroblock22@gmail.COM>
2023-12-28 15:55:42 +0000

    Refactor confirmations controller to redirect to frontend

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,11 +1,12 @@
 class ConfirmationsController < Devise::ConfirmationsController
     protected
 
-    def after_confirmation_path_for(resource_name, resource)
-        'localhost:3000/'
-    end
-end
+    # def after_confirmation_path_for(resource_name, resource)
+    #     'localhost:3000/'
+    # end
+    
+    def redirect_to_frontend
+        redirect_to "https://workout-app-fe-five.vercel.app/GettingStarted"
+      end
 
-def redirect_to_frontend
-    redirect_to "https://workout-app-fe-five.vercel.app/GettingStarted"
-  end
+end
