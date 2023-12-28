9807897e01a67b9d2456fe2c4d4442730f5c3bfb
Jethro_Block <jethroblock22@gmail.COM>
2023-12-28 11:11:25 +0000

    Add email validation and uniqueness to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable,
     :jwt_authenticatable, jwt_revocation_strategy: self
 
+    validates_format_of :email, with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"
+    validates :email, uniqueness: true
+    
     has_many :routines
     has_many :workouts
     has_many :workout_days
