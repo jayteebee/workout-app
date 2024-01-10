40a6d18d027cffccff7fdd013861c8027a0c7d8b
Jethro_Block <jethroblock22@gmail.COM>
2024-01-10 11:52:00 +0000

    Refactor workout_session_controller.rb

--- app/controllers/workout_session_controller.rb
+++ app/controllers/workout_session_controller.rb
@@ -1,79 +1,68 @@
 # class WorkoutSessionController < ApplicationController
 
-#     before_action :set_workout_session, only: [:show, :update, :destroy ]
-#     before_action :authenticate_user!
+#   before_action :set_workout_session, only: [:show, :update, :destroy ]
 
-#     # GET REQUESTS
-#     # /workout_session
-#     def index
-#         @workout_sessions = current_user.workout_sessions.all
-#         render json: @workout_sessions
-#     end
+#   # GET REQUESTS
+#   # /workout_session
+#   def index
+#       @workout_sessions = current_user.workout_sessions.all
+#       render json: @workout_sessions
+#   end
 
-#     # /workout_session/1
-#     def show
-#       if @workout_session.user_id == current_user.id
-#         render json: @workout_session
-#       else
-#         render json: {error: "Unauthorized"}, status: :Unauthorized
-#       end
-#     end
+#   # /workout_session/1
+#   def show
+#       render json: @workout_session
+#   end
 
-#     # POST REQUESTS
-#     # /workout_session
-#     def create
-#         @workout_session = current_user.workout_sessions.new(workout_session_params)
+#   # POST REQUESTS
+#   # /workout_session
+#   def create
+#       @workout_session = current_user.workout_sessions.new(workout_session_params)
 
-#         if @workout_session.save
-#             render json: @workout_session, status: :created
-#           else
-#             render json: @workout_session.errors, status: :unprocessable_entity
-#           end
-#     end
-
-
-#     # PATCH/PUT REQUESTS
-#     # /workout_session/1
-#     def update
-#       if @workout_session.user_id == current_user.id
-#         if @workout_session.update(workout_session_params)
-#             if @workout_session.total_duration
-#                 SessionLog.create!(
-#                   user_id: @workout_session.user_id,
-#                   details: @workout_session.as_json
-#                 )
-#               end
-#             render json: @workout_session, status: :ok
-#           else
-#             render json: @workout_session.errors, status: :unprocessable_entity
-#           end
+#       if @workout_session.save
+#           render json: @workout_session, status: :created
 #         else
-#           render json: { error: 'Unauthorized' }, status: :unauthorized
+#           render json: @workout_session.errors, status: :unprocessable_entity
 #         end
-#     end
+#   end
+
+
+#   # PATCH/PUT REQUESTS
+#   # /workout_session/1
+#   def update
+#       if @workout_session.update(workout_session_params)
+#           if @workout_session.total_duration
+#               SessionLog.create!(
+#                 user_id: @workout_session.user_id,
+#                 details: @workout_session.as_json,
+#                 workout_name: @workout_session.workout_name
+#               )
+#             end
+          
+
+#           render json: @workout_session, status: :ok
+#         else
+#           render json: @workout_session.errors, status: :unprocessable_entity
+#         end
+#   end
 
 # # DELETE REQUESTS   
 # # /workout_session/1
-#     def destroy
-#       if @workout_session.user_id == current_user.id
-#         @workout_session.destroy
-#       else 
-#         render json: { error: 'Unauthorized' }, status: :unauthorized
-#     end
+#   def destroy
+#       @workout_session.destroy
 #   end
 
-    
-#     private
+  
+#   private
 
-#     def set_workout_session
-#         @workout_session = WorkoutSession.find(params[:id])
+#   def set_workout_session
+#       @workout_session = current_user.workout_sessions.find(params[:id])
+#   end
+
+#   def workout_session_params
+#       params.require(:workout_session).permit(:user_id, :routine_workout_id, :date, :total_duration, :workout_name)
 #     end
-
-#     def workout_session_params
-#         params.require(:workout_session).permit(:user_id, :routine_workout_id, :date, :total_duration)
-#       end
 # end
-
 
 class WorkoutSessionController < ApplicationController
 
@@ -82,7 +71,7 @@ class WorkoutSessionController < ApplicationController
   # GET REQUESTS
   # /workout_session
   def index
-      @workout_sessions = current_user.workout_sessions.all
+      @workout_sessions = WorkoutSession.all
       render json: @workout_sessions
   end
 
@@ -94,7 +83,7 @@ class WorkoutSessionController < ApplicationController
   # POST REQUESTS
   # /workout_session
   def create
-      @workout_session = current_user.workout_sessions.new(workout_session_params)
+      @workout_session = WorkoutSession.new(workout_session_params)
 
       if @workout_session.save
           render json: @workout_session, status: :created
@@ -133,7 +122,7 @@ class WorkoutSessionController < ApplicationController
   private
 
   def set_workout_session
-      @workout_session = current_user.workout_sessions.find(params[:id])
+      @workout_session = WorkoutSession.find(params[:id])
   end
 
   def workout_session_params
